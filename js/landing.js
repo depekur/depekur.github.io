d4260ebbae5d716c6c58c01a55e81b7da8ed78e2
Evgeniy Pekur <depekur@gmail.com>
2015-06-19 18:09:38 +0300

    f

--- js/landing.js
+++ js/landing.js
@@ -1,32 +1,4 @@
 'use strict';
-/*
-* 	menu button
-*/
-
-$('#button').on('click', function() {
-  $( ".opacity-nav" ).fadeToggle( "fast", "linear" );
-});
-
-
-/*
-* 	parallax
-*/
-
-$('.sky').parallax({
-	imageSrc: 'media/landing/OSJQVTURTM.jpg',
-	speed: '0.3',
-	bleed: '1',
-});
-
-$('.landing-contact').parallax({
-	imageSrc: 'media/landing/4YCB0VAY1L.jpg',
-	speed: '0',
-	bleed: '1',
-	//androidFix: false,
-	//iosFix: false
-});
-
-
 
 /*
 * 	theater
@@ -60,3 +32,79 @@ theater
 	.write(function () { theater.play(true); });
 
 window.theater = theater;	
+
+
+/*
+* 	scroll & slider
+*/
+
+$(document).ready(function() {
+			$('#fullPage').fullpage({
+
+				verticalCentered: false,
+				continuousVertical: true,
+				css3: true,
+				scrollingSpeed: 700,
+				autoScrolling: true
+
+
+				/*
+				//Navigation
+				menu: false,
+				lockAnchors: false,
+				anchors:['firstPage', 'secondPage'],
+				navigation: false,
+				navigationPosition: 'right',
+				navigationTooltips: ['firstSlide', 'secondSlide'],
+				showActiveTooltip: false,
+				slidesNavigation: true,
+				slidesNavPosition: 'bottom',
+
+				//Scrolling
+				css3: true,
+				scrollingSpeed: 700,
+				autoScrolling: true,
+				fitToSection: true,
+				scrollBar: false,
+				easing: 'easeInOutCubic',
+				easingcss3: 'ease',
+				loopBottom: false,
+				loopTop: false,
+				loopHorizontal: true,
+				continuousVertical: false,
+				normalScrollElements: '#element1, .element2',
+				scrollOverflow: false,
+				touchSensitivity: 15,
+				normalScrollElementTouchThreshold: 5,
+
+				//Accessibility
+				keyboardScrolling: true,
+				animateAnchor: true,
+				recordHistory: true,
+
+				//Design
+				controlArrows: true,
+				verticalCentered: true,
+				resize : false,
+				sectionsColor : ['#ccc', '#fff'],
+				paddingTop: '3em',
+				paddingBottom: '10px',
+				fixedElements: '#header, .footer',
+				responsiveWidth: 0,
+				responsiveHeight: 0,
+
+				//Custom selectors
+				sectionSelector: '.section',
+				slideSelector: '.slide',
+
+				//events
+				onLeave: function(index, nextIndex, direction){},
+				afterLoad: function(anchorLink, index){},
+				afterRender: function(){},
+				afterResize: function(){},
+				afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+				onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+						*/
+
+			});
+		});
