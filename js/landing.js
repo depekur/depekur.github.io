bc176b8ef4479959a9c7d5e8f81375c3c7153f92
Evgeniy Pekur <depekur@gmail.com>
2015-07-10 06:30:44 +0300

    just do it!

--- js/landing.js
+++ js/landing.js
@@ -26,11 +26,12 @@ theater
 	.write("god: для твоего бизнеса", 2000, false)
 	.write(fuckTheKing, "god: которые решают задачи", 2000, fuckTheKing)				
 	.write("god: и их сопровождение", 2000, false)
-	.write("god: для производств", 2000, false)
+	.write("god: для B2B сегмента", 2000, false)
 	.write(fuckTheKing, "god: лендингов, магазинов", 2000, fuckTheKing)
-	//.write("god: для твоего магазина", 2000)
+	.write("god: для твоего магазина", 2000)
 	.write(fuckTheKing, "god: которые продают", 2000, fuckTheKing)	
 	.write("god: для твоей компании", 2000)
+	.write("god: для предпринимателей", 2000)
 	.write(function () { theater.play(true); });
 
 
