1760b189f3d212586b232c8438acf5d7a3fdb619
Evgeniy Pekur <depekur@gmail.com>
2015-07-17 00:16:48 +0300

    what do i doing whis my life?

--- js/landing.js
+++ js/landing.js
@@ -31,7 +31,7 @@ theater
 	.write("god: для твоего магазина", 2000)
 	.write(fuckTheKing, "god: которые продают", 2000, fuckTheKing)	
 	.write("god: для твоей компании", 2000)
-	.write("god: для предпринимателей", 2000)
+	//.write("god: для предпринимателей", 2000)
 	.write(function () { theater.play(true); });
 
 
@@ -41,11 +41,14 @@ function fuckTheKing() {
 
 
 
-/*
-* 	scroll & slider
-*/
+
 
 $(document).ready(function() {
+
+/*
+* 	scroll & slider (fullpage plugin)
+*/
+
 			$('#fullPage').fullpage({
 
 				verticalCentered: false,
@@ -73,6 +76,40 @@ $(document).ready(function() {
 			        }				
 
 			}); //fullpage END
-		});
+
+
+
+/*
+* 	Automatic Image Montage (portfolio section)
+*/
+			$(function() {
+
+				var $container 	= $('#banana-grid'),
+					$imgs		= $container.find('img').hide(),
+					totalImgs	= $imgs.length,
+					cnt			= 0;
+				
+				$imgs.each(function(i) {
+					var $img	= $(this);
+					$('<img/>').load(function() {
+						++cnt;
+						if( cnt === totalImgs ) {
+							$imgs.show();
+							$container.montage({
+								fillLastRow				: false,
+								alternateHeight			: true,
+								margin					: 5,	
+								alternateHeightRange	: {
+									min	: 100,
+									max	: 120
+								}
+							});
+		
+						}
+					}).attr('src',$img.attr('src'));
+				});	
+				
+			});
+		});//end doc.ready
 
 
